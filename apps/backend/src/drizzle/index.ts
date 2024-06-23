fa22291fd5b5a7d9030c4874e9f91371259ed4f6
Bastián <bparedesp@falabella.cl>
2024-06-23 15:41:38 -0400

    funciona

--- apps/backend/src/drizzle/index.ts
+++ apps/backend/src/drizzle/index.ts
@@ -5,8 +5,8 @@ import * as schema from './schema';
 
 const client = createClient({
   authToken:
-    'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MTkxMTk0NDUsImlkIjoiMDRjY2MxN2QtMWJjYS00ZTM0LWEwOTMtNmExZTI3NWYwNWQ5In0.XW-B2MOpci5DnlsULxjSG6FE-O9eSlUM88gK4OQGmq62qq2SbWdAWtDO4ShMIAJZOpu1iYr_j5jgKkN4m4L3BA',
-  url: 'libsql://ba-tester-bastianparedes.turso.io',
+    'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MTkxNzEwOTQsImlkIjoiNjViNTk5ODMtNjE5Zi00ZjEzLWEyNWItNjM3ZjkxZTljYTk0In0.HQSFyIFi3HDkk3DMnApn89yHmRhrpKZurXTcpOwsaB80GjX4R-J-0hvq2YzJBH6tbHqIca8ynw9WRIwfaeP1Bw',
+  url: 'libsql://tst-maker-bastianparedes.turso.io',
 });
 
 export default drizzle(client, { schema });
